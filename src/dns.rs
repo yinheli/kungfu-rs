b878cec932733c95aa64e176ea01089b2a8d46c3
yinheli <me@yinheli.com>
2021-03-26 01:36:20 +0800

    dns works!

--- src/dns.rs
+++ src/dns.rs
@@ -1,10 +1,12 @@
 use std::{
     borrow::Borrow,
     convert::TryInto,
-    net::{Ipv4Addr, SocketAddr, ToSocketAddrs},
+    iter,
+    net::{IpAddr, Ipv4Addr, SocketAddr, ToSocketAddrs},
     ops::Deref,
     pin::Pin,
     process,
+    slice::Iter,
     sync::Arc,
 };
 
@@ -18,7 +20,7 @@ use trust_dns_client::{
 };
 use trust_dns_proto::{op::header::MessageType, xfer::DnsRequestOptions};
 use trust_dns_resolver::{
-    config::{ResolverConfig, ResolverOpts},
+    config::{NameServerConfigGroup, ResolverConfig, ResolverOpts},
     IntoName, TokioAsyncResolver,
 };
 use trust_dns_server::{
@@ -96,7 +98,80 @@ impl QueryHandler {
     async fn query_upstream<R: ResponseHandler>(self, request: MessageRequest, response_handle: R) {
         // println!("query_upstream: {:?}", self.setting);
 
-        println!("request: {:#?}", request);
+        // println!("request: {:#?}", request);
+
+        let name_server_group =
+            NameServerConfigGroup::from_ips_clear(&[IpAddr::V4(Ipv4Addr::new(1, 2, 4, 8))], 53);
+        // let options = config.options.unwrap_or_default();
+        let config = ResolverConfig::from_parts(None, vec![], name_server_group);
+
+        let runtime = self.runtime.clone().handle().to_owned();
+        let resolver = TokioAsyncResolver::new(config, Default::default(), runtime)
+            .await
+            .unwrap();
+
+        let queries = request.queries();
+        let query = &queries[0];
+        let record_type = query.query_type();
+        let name = query.name();
+
+        let res = resolver
+            .lookup(name, record_type, Default::default())
+            .await
+            .unwrap();
+
+        println!("res: {:?}", res);
+        // let b = res.iter();
+        // let b = b.into_iter();
+
+        let mut answers = vec![];
+        let name_servers: Vec<&Record> = vec![];
+        let soa: Vec<&Record> = vec![];
+        let additionals: Vec<&Record> = vec![];
+        for x in res.record_iter() {
+            match x.record_type() {
+                RecordType::A => answers.push(x),
+                _ => {}
+            }
+        }
+
+        let mut header = Header::new();
+        header.set_id(request.id());
+        header.set_op_code(request.op_code());
+        header.set_message_type(MessageType::Response);
+
+        // let answers = Box::new(Arc::new(answers)).into_iter();
+        // let name_servers = Box::new(Arc::new(answers)).into_iter();
+        // let soa = Box::new(Arc::new(answers)).into_iter();
+        // let additionals = Box::new(Arc::new(answers)).into_iter();
+        // let answers = (Box::new(AuthLookup::default()) as Box<dyn LookupObject>).iter();
+        // let name_servers = (Box::new(AuthLookup::default()) as Box<dyn LookupObject>).iter();
+        // let soa = (Box::new(AuthLookup::default()) as Box<dyn LookupObject>).iter();
+        // let additionals = (Box::new(AuthLookup::default()) as Box<dyn LookupObject>).iter();
+        let builder = MessageResponseBuilder::new(Some(request.raw_queries()));
+
+        let a = res.record_iter();
+        let mut list = vec![];
+        for x in a {
+            list.push(x);
+        }
+        // let answers = Box::new(AuthLookup::default()) as Box<dyn LookupObject>;
+        // let name_servers = Box::new(AuthLookup::default()) as Box<dyn LookupObject>;
+        // let soa = Box::new(AuthLookup::default()) as Box<dyn LookupObject>;
+        // let additionals = Box::new(AuthLookup::default()) as Box<dyn LookupObject>;
+        // let answers = answers.iter();
+        // // let answers = None.into_iter();
+        // let name_servers = name_servers.iter();
+        // let soa = soa.iter();
+        // let additionals = additionals.iter();
+
+        let answers = Box::new(answers.into_iter()) as Box<dyn Iterator<Item = &Record> + Send>;
+        let name_servers = Box::new([].iter()) as Box<dyn Iterator<Item = &Record> + Send>;
+        let soa = Box::new([].iter()) as Box<dyn Iterator<Item = &Record> + Send>;
+        let additionals = Box::new([].iter()) as Box<dyn Iterator<Item = &Record> + Send>;
+        let response = builder.build(header, answers, name_servers, soa, additionals);
+        // println!("response: {:?}", response);
+        response_handle.send_response(response);
 
         // let queries = request.queries();
         // let query = &queries[0];
@@ -154,4 +229,26 @@ impl QueryHandler {
     //     let response = response_builder.build(header, a.iter(), b.iter(), c.iter(), d.iter());
     //     let _ = response_handle.send_response(response);
     // }
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_1() {
+        // let a: dyn IntoIterator<Item = Option> = Box::new(None).into_iter();
+        // let b: dyn IntoIterator<Item = Option, IntoIter = > = Box::new(None.into_iter());
+        // assert_eq!(a, b);
+
+        // let v = vec![Box::new(1)];
+        // let a = v.iter();
+        // for x in a {
+        //     println!("x:{}", x)
+        // }
+
+        // let b = v.into_iter();
+
+        // for x in b {
+        //     println!("x:{}", x)
+        // }
+    }
 }
