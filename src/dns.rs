9023d5fd9b3bad6bdd97ecdd027986321517d118
yinheli <me@yinheli.com>
2021-03-27 00:16:46 +0800

    update dns

--- src/dns.rs
+++ src/dns.rs
@@ -2,7 +2,7 @@ use std::{
     borrow::Borrow,
     convert::TryInto,
     iter,
-    net::{IpAddr, Ipv4Addr, SocketAddr, ToSocketAddrs},
+    net::{IpAddr, Ipv4Addr, ToSocketAddrs},
     ops::Deref,
     pin::Pin,
     process,
@@ -18,15 +18,13 @@ use trust_dns_client::{
     rr::{Record, RecordType},
     udp::{UdpClientConnection, UdpClientStream},
 };
-use trust_dns_proto::{op::header::MessageType, xfer::DnsRequestOptions};
+use trust_dns_proto::op::header::MessageType;
 use trust_dns_resolver::{
     config::{NameServerConfigGroup, ResolverConfig, ResolverOpts},
     IntoName, TokioAsyncResolver,
 };
 use trust_dns_server::{
-    authority::{
-        AuthLookup, LookupObject, MessageRequest, MessageResponse, MessageResponseBuilder,
-    },
+    authority::{MessageRequest, MessageResponseBuilder},
     server::{Request, RequestHandler, ResponseHandler},
     ServerFuture,
 };
@@ -46,7 +44,7 @@ pub async fn serve(setting: Arc<Setting>, runtime: Arc<Runtime>) {
             process::exit(1);
         }
     };
-    let rt = runtime.clone();
+    let _rt = runtime.clone();
     server.register_socket(socket, &runtime);
     debug!("dns server start");
     server.block_until_done().await.unwrap();
@@ -96,10 +94,6 @@ struct QueryHandler {
 
 impl QueryHandler {
     async fn query_upstream<R: ResponseHandler>(self, request: MessageRequest, response_handle: R) {
-        // println!("query_upstream: {:?}", self.setting);
-
-        // println!("request: {:#?}", request);
-
         let name_server_group =
             NameServerConfigGroup::from_ips_clear(&[IpAddr::V4(Ipv4Addr::new(1, 2, 4, 8))], 53);
         // let options = config.options.unwrap_or_default();
@@ -121,13 +115,11 @@ impl QueryHandler {
             .unwrap();
 
         println!("res: {:?}", res);
-        // let b = res.iter();
-        // let b = b.into_iter();
 
         let mut answers = vec![];
-        let name_servers: Vec<&Record> = vec![];
-        let soa: Vec<&Record> = vec![];
-        let additionals: Vec<&Record> = vec![];
+        let _name_servers: Vec<&Record> = vec![];
+        let _soa: Vec<&Record> = vec![];
+        let _additionals: Vec<&Record> = vec![];
         for x in res.record_iter() {
             match x.record_type() {
                 RecordType::A => answers.push(x),
@@ -140,14 +132,6 @@ impl QueryHandler {
         header.set_op_code(request.op_code());
         header.set_message_type(MessageType::Response);
 
-        // let answers = Box::new(Arc::new(answers)).into_iter();
-        // let name_servers = Box::new(Arc::new(answers)).into_iter();
-        // let soa = Box::new(Arc::new(answers)).into_iter();
-        // let additionals = Box::new(Arc::new(answers)).into_iter();
-        // let answers = (Box::new(AuthLookup::default()) as Box<dyn LookupObject>).iter();
-        // let name_servers = (Box::new(AuthLookup::default()) as Box<dyn LookupObject>).iter();
-        // let soa = (Box::new(AuthLookup::default()) as Box<dyn LookupObject>).iter();
-        // let additionals = (Box::new(AuthLookup::default()) as Box<dyn LookupObject>).iter();
         let builder = MessageResponseBuilder::new(Some(request.raw_queries()));
 
         let a = res.record_iter();
@@ -155,80 +139,14 @@ impl QueryHandler {
         for x in a {
             list.push(x);
         }
-        // let answers = Box::new(AuthLookup::default()) as Box<dyn LookupObject>;
-        // let name_servers = Box::new(AuthLookup::default()) as Box<dyn LookupObject>;
-        // let soa = Box::new(AuthLookup::default()) as Box<dyn LookupObject>;
-        // let additionals = Box::new(AuthLookup::default()) as Box<dyn LookupObject>;
-        // let answers = answers.iter();
-        // // let answers = None.into_iter();
-        // let name_servers = name_servers.iter();
-        // let soa = soa.iter();
-        // let additionals = additionals.iter();
 
         let answers = Box::new(answers.into_iter()) as Box<dyn Iterator<Item = &Record> + Send>;
         let name_servers = Box::new([].iter()) as Box<dyn Iterator<Item = &Record> + Send>;
         let soa = Box::new([].iter()) as Box<dyn Iterator<Item = &Record> + Send>;
         let additionals = Box::new([].iter()) as Box<dyn Iterator<Item = &Record> + Send>;
         let response = builder.build(header, answers, name_servers, soa, additionals);
-        // println!("response: {:?}", response);
-        response_handle.send_response(response);
-
-        // let queries = request.queries();
-        // let query = &queries[0];
-
-        // let address = "1.2.4.8:53".parse().unwrap();
-        // let conn = UdpClientConnection::new(address).unwrap();
-        // let client = SyncClient::new(conn);
-        // let name = query.name().into_name().unwrap();
-        // let result = client
-        //     .query(&name, query.query_class(), query.query_type())
-        //     .unwrap();
-        // let answers = result.answers().into_iter().collect::<Vec<Record>>();
-        // let additionals =
-        //     Box::new(result.additionals().into_iter().to_owned()) as Box<dyn Iterator<Item = _>>;
-        // let nameservers =
-        //     Box::new(result.name_servers().into_iter().to_owned()) as Box<dyn Iterator<Item = _>>;
-
-        // let mut header = Header::new();
-        // header.set_id(request.id());
-        // header.set_op_code(request.op_code());
-        // header.set_message_type(MessageType::Response);
-        // let response_builder = MessageResponseBuilder::new(Some(request.raw_queries()));
-        // let response = response_builder.build(header, answers, nameservers, answers, additionals);
-        // let _ = response_handle.send_response(response);
+        let _ = response_handle.send_response(response);
     }
-
-    // fn fun_name(self, request: MessageRequest, response_handle: R) {
-    //     let handle = self.runtime.handle().clone();
-    //     let resolver =
-    //         TokioAsyncResolver::new(ResolverConfig::google(), ResolverOpts::default(), handle)
-    //             .await
-    //             .unwrap();
-    //     let queries = request.queries();
-    //     let query = &queries[0];
-    //     let mut lookup_opt = DnsRequestOptions::default();
-    //     lookup_opt.expects_multiple_responses = true;
-    //     let vv = resolver.ns_lookup(query.name()).await.unwrap();
-    //     let f = resolver.lookup(query.name(), query.query_type(), lookup_opt);
-    //     let lookup = f.await.unwrap();
-    //     let response_builder = MessageResponseBuilder::new(Some(request.raw_queries()));
-    //     let mut header = Header::new();
-    //     header.set_id(request.id());
-    //     header.set_op_code(request.op_code());
-    //     header.set_message_type(MessageType::Response);
-    //     let a = Box::new(AuthLookup::default()) as Box<dyn LookupObject>;
-    //     let b = Box::new(AuthLookup::default()) as Box<dyn LookupObject>;
-    //     let c = Box::new(AuthLookup::default()) as Box<dyn LookupObject>;
-    //     let d = Box::new(AuthLookup::default()) as Box<dyn LookupObject>;
-    //     println!("lookup: {:#?}", lookup);
-    //     for v in lookup.into_iter() {
-    //         let a = v.to_record_type();
-    //         println!("--> {:?}", a);
-    //         println!("{:?}", v);
-    //     }
-    //     let response = response_builder.build(header, a.iter(), b.iter(), c.iter(), d.iter());
-    //     let _ = response_handle.send_response(response);
-    // }
 }
 
 #[cfg(test)]
